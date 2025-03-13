d629170e6181111da60bd477e77a245b0811bc80
TheKingMons <166251646+TheKingMons15@users.noreply.github.com>
2025-03-13 12:13:41 -0500

    Cambios

--- Fablab/src/App.tsx
+++ Fablab/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import fablab from "./Img/fablab.svg"
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,18 +9,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={fablab} className="logo react" alt="Fablab logo" />
         </a>
+
       </div>
-      <h1>Vite + React</h1>
+      <h1>Site Undercontroccion</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
