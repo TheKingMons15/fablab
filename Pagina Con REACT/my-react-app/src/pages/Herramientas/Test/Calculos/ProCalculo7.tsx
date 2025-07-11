cb7b18944979a2504b8004f2e6e891f5f674a5b0
Sebastian-Paucar-R <bryan.paucar@upec.edu.ec>
2025-07-11 16:40:24 -0500

    Correccion de test7

--- Pagina Con REACT/my-react-app/src/pages/Herramientas/Test/Calculos/ProCalculo7.tsx
+++ Pagina Con REACT/my-react-app/src/pages/Herramientas/Test/Calculos/ProCalculo7.tsx
@@ -279,29 +279,29 @@ const ProCalculo7: React.FC = () => {
       maxScore: 6,
       items: [
         { 
-          question: "Escribe el dónde colocarías el número 56 en una escala del 0 al 100 (elige 1, 2 o 3)", 
+          question: "¿Dónde colocarías el número 56 en una escala del 0 al 100? (elige 1, 2 o 3)", 
           answer: "2", 
           points: 2,
           type: "escrito",
           image: '/img/escala_56.png'
         },
         { 
-          question: "¿Dónde colocarías el número 80 en una escala del 0 al 100?", 
-          answer: "80", 
+          question: "¿Dónde colocarías el número 80 en una escala del 0 al 100? (elige 1, 2 o 3)", 
+          answer: "3", 
           points: 2,
           type: "escrito",
           image: '/img/escala_80.png'
         },
         { 
-          question: "¿Dónde colocarías el número 62 en una escala del 0 al 100?", 
-          answer: "62", 
+          question: "¿Dónde colocarías el número 62 en una escala del 0 al 100? (elige 1, 2 o 3)", 
+          answer: "2", 
           points: 2,
           type: "escrito",
           image: '/img/escala_62.png'
         },
         { 
           question: "¿Dónde colocarías el número 10 en una escala del 0 al 100?", 
-          answer: "10", 
+          answer: "1", 
           points: 2,
           type: "escrito",
           image: '/img/escala_10.png'
